a0f45bef78c494169ce52d3237e729a48080c436
Eunbyul <eunbyul0805@gmail.com>
2025-03-21 22:25:18 +0900

    Basic Mathematics_4

---
+++ Probability_Statistics/Sequence.py
@@ -0,0 +1,16 @@
+def seq(start, stop, step):
+  """
+  sequence function 
+  input : start(시작 값), stop(끝 값), step(한 스텝당 증가 수)
+  output: res(리스트)
+  """
+  res = []
+  current = start
+  while current < stop:
+    res.append(current)
+    current += step
+  return res
+
+
+start, stop, step = map(int, input().split())
+print(f"sequence(start:{start},stop:{stop},step:{step}): {seq(start, stop, step)}")
