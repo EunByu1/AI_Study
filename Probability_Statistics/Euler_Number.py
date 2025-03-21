2729a9bbfc13a3e35d84a604105a9b4e83ccdb1f
Eunbyul <eunbyul0805@gmail.com>
2025-03-21 22:19:26 +0900

    Create Euler_Number.py

---
+++ Probability_Statistics/Euler_Number.py
@@ -0,0 +1,34 @@
+def factorial(x):
+  """
+  factorial function
+  input : 정수 x
+  output: x!
+  """
+  x_list = list(range(1, x+1))
+  res    = 1
+  for val in x_list:
+    res *= val
+  return res 
+
+
+# ================== count 10 ==================
+n = 10
+e = 0
+
+for i in range(0,n):
+    e += 1/factorial(i)
+
+print(f"euler number(count 10): {e}")
+# ==============================================
+
+
+
+# ================== count 100 ==================
+n = 10
+e = 0
+
+for i in range(0,n):
+    e += 1/factorial(i)
+
+print(f"euler number(count 10): {e}")
+# ==============================================
