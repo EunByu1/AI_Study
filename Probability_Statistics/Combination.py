d4ea8d52ac0fcbb33db0f068e1806072cdab2f0c
Eunbyul <eunbyul0805@gmail.com>
2025-03-21 22:12:46 +0900

    Basic Mathematics_2

---
+++ Probability_Statistics/Combination.py
@@ -0,0 +1,25 @@
+def factorial(x):
+  """
+  factorial function
+  input : 정수 x
+  output: x!
+  """
+  x_list = list(range(1, x+1))
+  res    = 1
+  for val in x_list:
+    res *= val
+  return res 
+
+
+def combination(x, n):
+    """
+    combination function
+    input : 정수 x, n
+    output: 출력값: nCx(실수)
+    """
+    res = factorial(x)/(factorial(x-n)*factorial(n))
+    return res
+
+
+x, n = map(int, input().split())
+print(f"combination {x}C{n}: {combination(x,n)}")
