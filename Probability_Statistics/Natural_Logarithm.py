f555917f766c0866e4a62d0846cac07bd28cff3a
Eunbyul <eunbyul0805@gmail.com>
2025-03-21 22:44:39 +0900

    Basic Mathematics_5

--- Probability_Statistics/Natural_Logarithm.py
+++ Probability_Statistics/Natural_Logarithm.py
@@ -9,7 +9,6 @@ def log(x):
   res = n * (x ** (1/n) - 1)
   return res 
 
-
 x = int(input())
 print(f"natural logarithm(argument of the logarithm: {x}): {log(x)}")
 # ==============================================
